f1e934c454b5337839b188ee8dc9411d95915a8d
Santiago <santidasilva1408@gmail.com>
2022-08-06 00:29:33 -0300

    js file

--- Js/main/main.js
+++ Js/main/main.js
@@ -1,4 +1,3 @@
-
 gsap.from("#h-animation-x", {opacity: 0, x: 200,ease: "elastic.out(1,3)",duration: 1.5,delay:1});
 gsap.from("#h-animation-1", {opacity: 0,ease: "elastic.out(0.1,0.3)",y: -100, duration: 2,delay:0.5});
 gsap.from("#h-animation-2", {opacity: 0,ease: "elastic.out(0.5,0.3)",x: -300,duration: 2});
